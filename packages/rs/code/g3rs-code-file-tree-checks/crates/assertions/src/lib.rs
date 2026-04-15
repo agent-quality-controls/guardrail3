27b796735cb36f36a64ee972ba065edcea0e37ff
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2026-04-15 21:57:48 +0100

    Clean code file tree checks package

--- packages/rs/code/g3rs-code-file-tree-checks/crates/assertions/src/lib.rs
+++ packages/rs/code/g3rs-code-file-tree-checks/crates/assertions/src/lib.rs
@@ -1,3 +1,8 @@
-mod common;
+#![allow(
+    clippy::missing_docs_in_private_items,
+    reason = "assertions scaffold will gain rule-specific helpers later"
+)]
 
-pub use common::{ExpectedRuleResult, assert_rule_results};
+#[cfg(feature = "checks")]
+use g3rs_code_file_tree_checks_runtime as _;
+use guardrail3_check_types as _;
